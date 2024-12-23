8597a59d8a586cb01acf2be88f3e7692700d5b89
SIlD07 <sychilia07@gmail.com>
2024-12-23 18:37:21 +0300

    started working on counter

--- SuggestContinueScreen.py
+++ SuggestContinueScreen.py
@@ -6,7 +6,12 @@ class ContinueScreen:
         self.screen = screen
         self.text_font = pygame.font.Font(None, 150)
         self.continue_text = self.text_font.render("Continue?", True, (0, 0, 0))
+        self.counter = 999
+        self.counter_text = self.text_font.render(str(self.counter // 100), True, (0, 0, 0))
 
-    def run(self) -> None:
+    def update(self) -> None:
         self.screen.fill((255, 255, 255))
         self.screen.blit(self.continue_text, (250, 100))
+        self.counter -= 1
+        self.counter_text = self.text_font.render(str(self.counter // 100), True, (0, 0, 0))
+        self.screen.blit(self.counter_text, (450, 250))
